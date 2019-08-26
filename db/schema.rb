bac2891b84244cd781c4326650b6ae1b69af498a
nakashima <nakashima@biocomputing.cc>
2019-08-26 23:19:15 +0900

    nameカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_131241) do
+ActiveRecord::Schema.define(version: 2019_08_26_141629) do
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "isbn"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_08_26_131241) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
