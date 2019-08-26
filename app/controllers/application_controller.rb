9c9715d0582e04b27d1c031e20222bcf1ea2ec7f
nakashima <nakashima@biocomputing.cc>
2019-08-26 23:44:54 +0900

    signup完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    lists_path # ログイン後に遷移するpathを設定
+  end
+  def after_sign_out_path_for(resource)
+    root_path # ログアウト後に遷移するpathを設定
+  end
 end
