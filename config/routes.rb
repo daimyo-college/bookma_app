c022274a7be48451ae95b594ea6cd90d90a9ca45
nakashima <nakashima@biocomputing.cc>
2019-08-26 23:09:40 +0900

    最低限のサインアップフォームを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root 'homes#index'
   resources :books
   resources :lists
+  # get "/users/sign_up", to: "registors#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
