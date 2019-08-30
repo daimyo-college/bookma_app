6a87b59fa0a0ba763bdea3d785f943c5dff5812b
rines <rines03i@outlook.com>
2019-08-31 02:03:00 +0900

    authorのデータ型をtextに変更”

--- db/migrate/20190827234954_add_column_to_book.rb
+++ db/migrate/20190827234954_add_column_to_book.rb
@@ -1,6 +1,6 @@
 class AddColumnToBook < ActiveRecord::Migration[5.2]
   def change
-    add_column :books, :author, :string
+    add_column :books, :author, :text
     add_column :books, :imageurl, :string
     add_column :books, :caption, :text
   end
