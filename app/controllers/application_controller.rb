13f7a2ac3ebd6c7a5fbef0486f9f2e1fca4d77d2
nakashima <nakashima@biocomputing.cc>
2019-09-09 01:14:35 +0900

    リスト編集のアクセス制御、フラッシュメッセージの表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,11 @@ class ApplicationController < ActionController::Base
     end
 
     def after_sign_in_path_for(resource)
-      lists_path # ログイン後に遷移するpathを設定
+      lists_path 
     end
     
     def after_sign_out_path_for(resource)
-      root_path # ログアウト後に遷移するpathを設定
+      root_path 
     end
 
   private
